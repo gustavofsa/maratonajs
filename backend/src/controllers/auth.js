11da21c2796b92bdee6d2a1dccd469bf70281b80
Gustavo Farias <gugafarias2009@gmail.com>
2020-06-30 11:06:37 -0300

    Add: Account sign up storing in db

--- backend/src/controllers/auth.js
+++ backend/src/controllers/auth.js
@@ -12,15 +12,15 @@ router.get('/sign-in', (req, res) => {
 });
 
 router.get('/sign-up', async (req, res) => {
-  const email = 'gugafarias2009@gmail.com';
-  const password = '123456';
+  const { email, password } = req.body;
+
+  const account = await Account.findOne({ where: { email } });
+  if (account) return res.json('Account already exists');
 
   const hash = bcrypt.hashSync(password, saltRounds);
-  console.log(hash)
+  const newAccount = await Account.create({ email, password: hash });
 
-  const result = await Account.create({ email, password: hash });
-
-  return res.json(result);
+  return res.json(newAccount);
 });
 
 module.exports = router;
